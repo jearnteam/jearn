beba8888c350a61f90791f34c45131e7efb71fa7
Kioh21 <kaungmyat.aung217@gmail.com>
2025-10-13 20:53:30 +0900

    Stop tracking next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-import path from "path";
-
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
-  allowedDevOrigins: ["https://kioh.jearn.site", "https://ssh.jearn.site"],
-};
-
-export default nextConfig;
