b37f40892479da4d9972c5dcdd1402cecf77f6ed
Kioh21 <kaungmyat.aung217@gmail.com>
2025-10-13 23:05:11 +0900

    three ajust

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
     <header className="fixed top-0 left-0 w-full bg-white border-b shadow-sm z-50">
       <div className="max-w-5xl mx-auto flex justify-between items-center px-4 py-3 text-black">
         <div className="flex items-center gap-3">
-          <div className="w-8 h-8 flex items-center justify-center">
+          <div className="w-12 h-8 flex items-center justify-center">
             <ThreeBall />
           </div>
           <h1
