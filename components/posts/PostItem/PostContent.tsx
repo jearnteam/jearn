00151308fe9183dcf744a7463b75570f8136ec0c
Kioh21 <kaungmyat.aung217@gmail.com>
2025-12-15 13:10:37 +0900

    PostItemのタイトルにリンクを追加

--- components/posts/PostItem/PostContent.tsx
+++ components/posts/PostItem/PostContent.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { MathRenderer } from "@/components/math/MathRenderer";
 import { usePostCollapse } from "./usePostCollapse";
+import { PostTypes } from "@/types/post";
+import Link from "next/link";
 
 export default function PostContent({
   post,
@@ -20,11 +22,14 @@ export default function PostContent({
 
   return (
     <>
-      {post.title && (
-        <h2 className="font-semibold text-lg text-gray-800 dark:text-gray-100">
-          {post.title}
-        </h2>
-      )}
+      <Link href={`/posts/${post._id}`} scroll={false}>
+        {post.title && (
+          <h2 className="font-semibold text-lg text-gray-800 dark:text-gray-100">
+            {post.postType === PostTypes.QUESTION ? <span className="text-red-500">{"Q. "}</span> : ""}
+            {post.title}
+          </h2>
+        )}
+      </Link>
 
       <motion.div
         animate={{ height: targetHeight }}
